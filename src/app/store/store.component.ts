f24866f8d806bfb7945619e135d33ec13f042a40
Anish Mukherjee <mukherjee.an@northeastern.edu>
2020-12-27 15:01:54 -0500

    added navigation tools

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,14 +8,42 @@ import {ProductRepository} from '../model/product.repository';
 })
 export class StoreComponent {
 
+    public selectedCategory = null;
+
+    // variables used to split displayed product results over multiple pages
+    public productsPerPage = 4;
+    public selectedPage = 1;
+
     constructor(private repository: ProductRepository) {
     }
 
     get products(): Product[] {
-        return this.repository.getProducts();
+        // get the lower bound index to display products from
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[] {
         return this.repository.getCategories();
     }
+
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number) {
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
+    }
+
+    // this is a method to return a range of page numbers, required to display navigation buttons
+    get pageNumbers(): number[] {
+        // create an array of the correct size and fill it with increasing integers
+        return Array(Math.ceil(this.repository.getProducts().length / this.productsPerPage)).fill(0).map((x, i) => i + 1);
+    }
+
 }
